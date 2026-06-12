30815f013a74a1e4ab146959e5890f0f73b9f671
munenick <mune0127@munenick.me>
2026-06-12 19:08:06 +0900

    fix: support button asChild

--- registry/react/ui/button.tsx
+++ registry/react/ui/button.tsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { Button as ButtonPrimitive } from "@base-ui/react/button"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -38,17 +39,27 @@ const buttonVariants = cva(
 )
 
 function Button({
+  asChild = false,
+  children,
   className,
   variant = "default",
   size = "default",
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonPrimitive.Props &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const render = asChild && React.isValidElement(children) ? children : undefined
+
   return (
     <ButtonPrimitive
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      render={render}
       {...props}
-    />
+    >
+      {render ? undefined : children}
+    </ButtonPrimitive>
   )
 }
 
